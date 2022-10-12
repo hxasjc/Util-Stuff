af5edaf51bc0bb7725371f13a9050a3442cdf600
hxasjc <karlvonweyhe@gmail.com>
2022-10-12 16:03:10 -0400

    javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"hxasjc.utilstuff.data","l":"HashMapCounter"},{"p":"hxasjc.utilstuff.data","l":"NumberConverter"}];updateSearchResults();
